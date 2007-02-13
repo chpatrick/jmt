a6dbb60a6b5b0f1b13fce34da44215c320420955
gourry <gourry@99a8c20d-b106-0410-ac6d-a91ca9afac8a>
2007-02-13 19:56:03 +0000

    Merged with branch to support transforms
    git-svn-id: svn://svn.code.sf.net/p/jmt/code/trunk@331 99a8c20d-b106-0410-ac6d-a91ca9afac8a

--- src/jmt/framework/gui/image/ImageLoader.java
+++ src/jmt/framework/gui/image/ImageLoader.java
@@ -18,7 +18,9 @@
 package jmt.framework.gui.image;
 
 import java.awt.Dimension;
+import java.awt.Graphics2D;
 import java.awt.Image;
+import java.awt.image.BufferedImage;
 import java.net.URL;
 import java.util.HashMap;
 
@@ -37,6 +39,7 @@ import javax.swing.ImageIcon;
  * @version 1.0
  */
 public abstract class ImageLoader {
+    public static final String MODIFIER_MIRROR = "MIRROR";
     private static final String[] EXTENSIONS = new String[] {".gif", ".png", ".jpg"};
     /** Maximum number of cached elements */
     protected int maxCache = 256; 
@@ -56,26 +59,50 @@ public abstract class ImageLoader {
      * @return icon if found, null otherwise
      */
     public ImageIcon loadIcon(String iconName) {
+        return this.loadIcon(iconName, (String)null);
+    }
+
+    /**
+     * Loads an icon with specified name and specified modifier and caches it.
+     * @param iconName name of the icon to be loaded
+     * @param modifier modifier to be applied (can be null)
+     * @return icon if found, null otherwise
+     */
+    public ImageIcon loadIcon(String iconName, String modifier) {
         if (iconName == null)
             return null;
-        if (iconCache.containsKey(iconName))
-            return (ImageIcon)iconCache.get(iconName);
-        
-        URL url = getImageURL(iconName);
+        String derivedName = iconName;
+        // Apply modifier to icon name if needed
+        if (modifier != null) {
+            derivedName = deriveIconName(iconName, modifier);
+        }
+
+        if (iconCache.containsKey(derivedName))
+            return (ImageIcon)iconCache.get(derivedName);
+
+        URL url = getImageURL(derivedName);
         // If image is not found, try to add extensions
         for (int i=0; i<EXTENSIONS.length && url == null; i++) {
-            url = getImageURL(iconName + EXTENSIONS[i]);
+            url = getImageURL(derivedName + EXTENSIONS[i]);
         }
-        
+
         ImageIcon tmp = null;
-        if (url != null)
+        if (url != null) {
             tmp = new ImageIcon(url);
-        
+        } else if (modifier != null) {
+            // Loads base icon without modifiers
+            ImageIcon base = loadIcon(iconName);
+            // Apply known modifiers
+            if (modifier.equals(MODIFIER_MIRROR)) {
+                tmp = mirrorImage(base);
+            }
+        }
+
         // Clears cache when it's bigger than maxCache. This is not needed but avoids memory leakages...
         if (iconCache.size() > maxCache)
             iconCache.clear();
-        
-        iconCache.put(iconName, tmp);
+
+        iconCache.put(derivedName, tmp);
         return tmp;
     }
     
@@ -95,4 +122,30 @@ public abstract class ImageLoader {
         else 
             return im;
     }
+
+    /**
+     * Derives an icon name to apply suffixes before extension
+     * @param iconName name of the icon to be derived
+     * @param modifier suffix to be appended
+     * @return derived name
+     */
+    public static String deriveIconName(String iconName, String modifier) {
+        int dot = iconName.lastIndexOf('.');
+        if (dot < 0) {
+            return iconName + modifier;
+        }
+        else {
+            return iconName.substring(0, dot) + modifier + iconName.substring(dot);
+        }
+    }
+
+    // --- Methods to apply modifiers to loaded icons ---------------------------------------------------
+    protected ImageIcon mirrorImage(ImageIcon base) {
+        BufferedImage out = new BufferedImage(base.getIconWidth(),base.getIconHeight(), BufferedImage.TYPE_INT_ARGB);
+        Graphics2D g = out.createGraphics();
+        g.drawImage(base.getImage(), base.getIconWidth(), 0, 0, base.getIconHeight(),
+                0, 0, base.getIconWidth(),base.getIconHeight(), null);
+        return new ImageIcon(out);
+    }
+    // --------------------------------------------------------------------------------------------------
 }
