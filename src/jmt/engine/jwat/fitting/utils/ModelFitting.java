c155991e05cd5a7c402a8f9f3dbe1b92bbedbda8
gourry <gourry@99a8c20d-b106-0410-ac6d-a91ca9afac8a>
2010-08-13 08:19:19 +0000

    Code was not compiling on Java 1.5 .
    git-svn-id: svn://svn.code.sf.net/p/jmt/code/trunk@568 99a8c20d-b106-0410-ac6d-a91ca9afac8a

--- src/jmt/engine/jwat/fitting/utils/ModelFitting.java
+++ src/jmt/engine/jwat/fitting/utils/ModelFitting.java
@@ -10,7 +10,7 @@ import jmt.engine.jwat.workloadAnalysis.utils.SetMatrixListener;
 import jmt.gui.jwat.JWatModel;
 
 public class ModelFitting implements JWatModel {
-	
+
 	// Matrice delle osservazioni
 	private MatrixOsservazioni matrix = null;
 	//array of observations (only numerical for fitting)
@@ -20,36 +20,33 @@ public class ModelFitting implements JWatModel {
 	// vector of the listener on set matrix
 	private Vector<SetMatrixListener> listenerOnMatrixChange = null; //<SetMatrixListener> 
 	private Vector<ChangeVariableListener> listenerOnChangeVariable = null; //<ChangeVariableListener> 
-	
+
 	public ModelFitting() {
 		listenerOnMatrixChange = new Vector<SetMatrixListener>();
 		listenerOnChangeVariable = new Vector<ChangeVariableListener>();
 	}
 
-	@Override
 	public MatrixOsservazioni getMatrix() {
 		// TODO Auto-generated method stub
 		return matrix;
 	}
 
-	@Override
 	public void resetModel() {
 		matrix = null;
 		fireNotifyOnResetMatrixObservation();
 	}
 
-	@Override
 	public void setMatrix(MatrixOsservazioni matrix) {
 		Observation[] obs;
 		int i;
-		this.matrix =  matrix;
-		
+		this.matrix = matrix;
+
 		//fill the list of double
 		obs = matrix.getListOss();
 		obslist = new double[obs.length];
-		
+
 		i = 0;
-		for(Observation o:obs) {
+		for (Observation o : obs) {
 			obslist[i] = o.getIndex(0);
 			i++;
 		}
@@ -59,11 +56,11 @@ public class ModelFitting implements JWatModel {
 		//*****************************
 		fireNotifyOnSetMatrixObservation();
 	}
-	
+
 	public double[] getListObservations() {
 		return obslist;
 	}
-	
+
 	/*
 	 * Notify change on matrix observation to all registered listener 
 	 */
@@ -72,13 +69,13 @@ public class ModelFitting implements JWatModel {
 			listenerOnMatrixChange.get(i).onSetMatrixObservation();
 		}
 	}
-	
+
 	private void fireNotifyOnResetMatrixObservation() {
 		for (int i = 0; i < listenerOnMatrixChange.size(); i++) {
 			listenerOnMatrixChange.get(i).onResetMatrixObservation();
 		}
 	}
-	
+
 	/**
 	 *
 	 *@param listener
@@ -86,64 +83,64 @@ public class ModelFitting implements JWatModel {
 	public void addOnSetMatrixObservationListener(SetMatrixListener listener) {
 		listenerOnMatrixChange.add(listener);
 	}
-	
+
 	public void addOnChangeVariableValue(ChangeVariableListener listener) {
 		if (!listenerOnChangeVariable.contains(listener)) {
 			listenerOnChangeVariable.add(listener);
 		}
 	}
-	
+
 	public double getMean() {
 		return statValues[0];
 	}
-	
+
 	public double getVariance() {
 		return statValues[1];
 	}
-	
+
 	public double getCoeffVariation() {
 		return statValues[2];
 	}
-	
+
 	public double getMin() {
 		return obslist[0];
 	}
-	
+
 	public double getMax() {
-		return obslist[obslist.length-1];
+		return obslist[obslist.length - 1];
 	}
-	
+
 	public double getRange() {
-		return obslist[obslist.length-1] - obslist[0];
+		return obslist[obslist.length - 1] - obslist[0];
 	}
-	
+
 	public double[] calculateStatisticalValues(double[] data) {
 		double sum = 0;
 		//double mean;
 		//double variance;
 		double[] result;
-		
+
 		result = new double[3];
-		
-		for(int i=0;i<data.length;i++) {
-			sum += data[i];
+
+		for (double element : data) {
+			sum += element;
 		}
-		
-		result[0] = sum/data.length;
+
+		result[0] = sum / data.length;
 		//System.out.println("Media:" + result[0]);
 		result[1] = 0;
-		
-		for(int i=0;i<data.length;i++) {
-			result[1] += Math.pow(data[i] - result[0],2);
+
+		for (double element : data) {
+			result[1] += Math.pow(element - result[0], 2);
 		}
-		
+
 		result[1] /= (data.length - 1);
 		//System.out.println("Varianza: " + result[1]);
 		result[2] = Math.sqrt(result[1]) / result[0];
-		
+
 		//System.out.println("Coefficient of variation: "+result[2]);
-		
+
 		return result;
-		
+
 	}
 }
